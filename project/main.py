a938e59fa2b9c9503e4fd947caaee102397963ef
Mubashir-19 <64585653+Mubashir-19@users.noreply.github.com>
2024-06-05 11:07:40 +0500

    Update main.py

--- project/main.py
+++ project/main.py
@@ -11,12 +11,33 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential()
+# Create a Sequential model
+model = models.Sequential([
 
-model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(128,activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    # First convolutional layer: 32 filters, 3x3 size, ReLU activation function
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+
+    # First max pooling layer: 2x2 size
+    layers.MaxPooling2D((2, 2)),
+
+    # Second convolutional layer: 64 filters, 3x3 size, ReLU activation function
+    layers.Conv2D(64, (3, 3), activation='relu'),
+
+    # Second max pooling layer: 2x2 size
+    layers.MaxPooling2D((2, 2)),
+
+    # Third convolutional layer: 64 filters, 3x3 size, ReLU activation function
+    layers.Conv2D(64, (3, 3), activation='relu'),
+
+    # Flatten the feature maps into a single 1D vector
+    layers.Flatten(),
+
+    # Fully connected (dense) layer with 64 units and ReLU activation function
+    layers.Dense(64, activation='relu'),
+
+    # Output layer with 10 units (one for each digit) and softmax activation function
+    layers.Dense(10, activation='softmax')
+])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=3)
